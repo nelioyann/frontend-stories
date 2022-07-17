b2619ec8eb7a9ff608f6d09e3c716678e28894e8
nelioyann <nanayannick1998@gmail.com>
2022-07-17 23:30:10 +0200

    feat - display stories

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
+  // Watch css files for changes
+  eleventyConfig.addWatchTarget("./src/css/**/*.css");
   // eleventyConfig.addPassthroughCopy("./src/scripts");
   
   return {
