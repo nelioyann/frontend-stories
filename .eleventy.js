505b17ed2cc8b0b8f15577dec51dbdd6bd859a21
nelioyann <nanayannick1998@gmail.com>
2022-07-20 23:33:17 +0200

    feat - add md rendering and related stories filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,9 @@
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const getSimilarCategoriesCount = function(categoriesA, categoriesB) {
+  let categoriesNamesA = categoriesA.map((category) => category.name);
+  let categoriesNamesB = categoriesB.map((category) => category.name);
+  return categoriesNamesA.filter(c => categoriesNamesB.includes(c)).length;
+}
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/images");
@@ -6,7 +12,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/css/**/*.css");
   // eleventyConfig.addPassthroughCopy("./src/scripts");
   // Create collection from _data/customData.js
-
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addCollection("stories", (collection) => {
     // const slug = eleventyConfig.getFilter("slugify");
     return collection.getAll()[0].data.stories;
@@ -17,6 +23,15 @@ module.exports = function (eleventyConfig) {
     //   };
     // });
   });
+
+  eleventyConfig.addFilter("similarStories", function(stories, currentStoryId, categories){
+    return stories.filter((story) => {
+      return getSimilarCategoriesCount(categories, story.categories) >= 1 && story.id !== currentStoryId;
+    }).sort((a,b) => {
+      return getSimilarCategoriesCount(b.categories, categories) - getSimilarCategoriesCount(a.categories, categories);
+    });
+  });
+
   return {
     dir: {
       input: "src",
