525688e3ccf1a215b91d0ba913df915ce819cad3
nelioyann <nanayannick1998@gmail.com>
2022-08-01 02:24:48 +0200

    feat - add emji favicon 👀

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,9 +1,10 @@
 module.exports = {
     name: "Frontend Stories",
     site_name: "Frontend Stories",
+    site_description: "Collection of reasearch-backed frontend guidelines and recommendations! ",
+    site_url: "https://frontend-stories.yannicknana.fr",
+    site_thumbnail: "https://frontend-stories.yannicknana.fr/images/thumbnail.png",
     theme_color: "#000",
     primary_color: "#fda312",
     twitter_name: "@nelioyann",
-    site_url: "https://frontend-stories.yannicknana.fr",
-    site_thumbnail: "https://frontend-stories.yannicknana.fr/images/thumbnail.png"
 }
