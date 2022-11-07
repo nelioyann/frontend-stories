6567a62b19b8b6e4d8b507d13b5d4ea28cbd1abd
nelioyann <nanayannick1998@gmail.com>
2022-11-08 00:25:40 +0100

    fix: 🐛 handle undefined env value

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,14 +1,9 @@
-// require("dotenv").config();
-// const ELEVENTY_ENV  = process.env.ELEVENTY_ENV.trim();
-
-// console.log(process.env.ELEVENTY_ENV.trim() + 'development')
-// console.log(ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://frontendstories.com')
 module.exports = {
     name: "Frontend Stories",
     site_name: "Frontend Stories",
     site_emoji: "🎓",
     site_description: "Frontend Stories is intended for developers and aims to facilitate access to research data, as well as facts, quotes and articles on which they can build their own web development guidelines.",
-    site_url: process.env.ELEVENTY_ENV.trim() === 'development' ? 'http://localhost:8080' : 'https://frontendstories.com',
+    site_url: (process.env.ELEVENTY_ENV || "").trim() === 'development' ? 'http://localhost:8080' : 'https://frontendstories.com',
     site_thumbnail: "https://frontendstories.com/images/thumbnail.png",
     theme_color: "#FAFAFA",
     logo_backgroud_color: "#FAFAFA",
