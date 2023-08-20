2bf0af345485d76704aa1627cde7bfa62224f70e
nelioyann <nanayannick1998@gmail.com>
2023-08-20 11:38:37 +0200

    feat: Code split theme toggle

---
+++ src/_includes/partials/site-theme-toggle.js
@@ -0,0 +1,17 @@
+// PE: Add element when JS available
+const toggleTemplate = document.querySelector(".theme-toggle-template");
+const toggleContent = toggleTemplate.content.cloneNode(true);
+toggleTemplate.replaceWith(toggleContent);
+const toggle = document.querySelector(".theme-toggle");
+// Listen to theme changes and update toggle
+document.addEventListener("themechange", (event) => {
+	toggle.setAttribute("aria-pressed", event.detail === "dark");
+});
+// Listen to user toggle changes
+toggle.addEventListener("click", function (event) {
+	let theme =
+		document.documentElement.getAttribute("data-theme-preference") === "dark"
+			? "light"
+			: "dark";
+	document.dispatchEvent(new CustomEvent("themechange", { detail: theme }));
+});
